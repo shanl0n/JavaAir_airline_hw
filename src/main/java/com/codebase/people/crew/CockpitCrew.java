18ce5682a833118e29dedc9084a6e3ed1fc348a4
Sean Hanlon <sean.a.hanlon@gmail.com>
2023-09-23 22:06:48 +0100

    "Added functioning CockpitCrewTest"

--- src/main/java/com/codebase/people/crew/CockpitCrew.java
+++ src/main/java/com/codebase/people/crew/CockpitCrew.java
@@ -2,7 +2,7 @@ package com.codebase.people.crew;
 
 public class CockpitCrew extends FlightCrew{
 
-	private String licenceNumber;
+	private final String licenceNumber;
 
 	public CockpitCrew(CrewRank rank, String name, String licenceNumber) {
 		super(rank, name);
@@ -12,6 +12,8 @@ public class CockpitCrew extends FlightCrew{
 	public String getLicenceNumber() {
 		return licenceNumber;
 	}
+
+
 
 	public String takeoff() {
 		return "I believe I can fly!";
---
+++ src/test/java/CockpitCrewTest.java
@@ -0,0 +1,34 @@
+import com.codebase.people.crew.CockpitCrew;
+import com.codebase.people.crew.CrewRank;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CockpitCrewTest {
+
+	CockpitCrew cockpitCrew;
+
+	@Before
+	public void before() { cockpitCrew = new CockpitCrew(CrewRank.CAPTAIN, "Jack Sparrow", "P1lot");}
+
+	@Test
+	public void hasRank() {
+		assertEquals(CrewRank.CAPTAIN, cockpitCrew.getRank());
+	}
+
+	@Test
+	public void hasName() {
+		assertEquals("Jack Sparrow", cockpitCrew.getName());
+	}
+
+	@Test
+	public void hasLicence() {
+		assertEquals("P1lot", cockpitCrew.getLicenceNumber());
+	}
+
+	@Test
+	public void canTakeoff() {
+		assertEquals("I believe I can fly!", cockpitCrew.takeoff());
+	}
+}
